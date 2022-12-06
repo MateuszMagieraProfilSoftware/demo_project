9a7b89fa35f748527e48a959b2db9cbf64f2263a
Mateusz <mateusz.magiera@profil-software.com>
2022-12-06 12:35:00 +0100

    separating rock,paper and scissors to separate classes

--- game/rock_paper_scissors.py
+++ game/rock_paper_scissors.py
@@ -27,6 +27,7 @@ class Rock:
         weak_against = 'Paper'
         return strong_against,weak_against
 
+
 class Scissors:
 
     @staticmethod
@@ -37,40 +38,11 @@ class Scissors:
 
 
 
-class Scissors:
-
-    def __int__(self,strong_against,weak_against):
-        self.strong_against = strong_against
-        self.weak_against = weak_against
-
-
-class BasicFiguresAttributes:
-
-    def __int__(self):
-        pass
-
-    @staticmethod
-    def paper():
-        strong_against = "Rock"
-        weak_against = "Scissors"
-        return strong_against,weak_against
-
-    @staticmethod
-    def rock():
-        strong_against = "Scissors"
-        weak_against = "Paper"
-        return strong_against,weak_against
-
-    @staticmethod
-    def scissors():
-        strong_against = "Paper"
-        weak_against = "Rock"
-        return strong_against,weak_against
-
 
 class UserChoice(Rock,Paper,Scissors):
 
-    def user_choice(self):
+    @staticmethod
+    def user_choice():
         user_choice = input("What mode would you prefer basic or advanced? :").capitalize()
         if user_choice == "Basic":
             print("You are playing a basic mode, please select one of the three figures "
@@ -81,15 +53,15 @@ class UserChoice(Rock,Paper,Scissors):
             if user_figure == "Rock":
                 print("You selected rock")
                 print("A rock is strong against " + Rock.rock_attributes()[0])
-                print("A rock is weak against " + BasicFiguresAttributes.rock()[1])
+                print("A rock is weak against " + Rock.rock_attributes()[1])
             elif user_figure == "Paper":
                 print("You selected paper")
-                print("The paper is strong against " + BasicFiguresAttributes.paper()[0])
-                print("The paper is weak against " + BasicFiguresAttributes.paper()[1])
+                print("The paper is strong against " + Paper.paper_attributes()[0])
+                print("The paper is weak against " + Paper.paper_attributes()[1])
             elif user_figure == "Scissors":
                 print("You selected scissors")
-                print("Scissors are strong against " + BasicFiguresAttributes.scissors()[0])
-                print("Scissors are weak against " + BasicFiguresAttributes.scissors()[1])
+                print("Scissors are strong against " + Scissors.scissors_attributes()[0])
+                print("Scissors are weak against " + Scissors.scissors_attributes()[1])
             else:
                 raise RuntimeError("Incorrect figure")
         elif user_choice == "Advanced":
@@ -100,5 +72,5 @@ class UserChoice(Rock,Paper,Scissors):
 
 
 WelcomeUser.welcome_user()
-UserChoice.user_choice(self=None)
+UserChoice.user_choice()
 
