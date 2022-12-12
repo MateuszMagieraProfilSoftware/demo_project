920d391ca8e03f55005e6623f76959d428beea55
Mateusz <mateusz.magiera@profil-software.com>
2022-12-12 14:59:03 +0100

    updating rock_paper_scissors_v2

--- game/rock_paper_scissors_v2.py
+++ game/rock_paper_scissors_v2.py
@@ -12,60 +12,36 @@ class FigureName(Enum):
     ROCK = "ROCK"
     PAPER = "PAPER"
     SCISSORS = "SCISSORS"
+    LIZARD = "LIZARD"
+    SPOCK = "SPOCK"
 
 
-class Rock:
-    figure_name = FigureName.ROCK.value
-    strong_against = [FigureName.SCISSORS.value]
-    weak_against = [FigureName.PAPER.value]
+class Figure:
+    figure_name : FigureName
+    strong_against: List[FigureName]
 
-    def fight(self, other_figure):
-        if other_figure in Rock.strong_against:
-            return Result.WIN
-        elif other_figure == Rock.figure_name:
-            return Result.TIE
-        elif other_figure in Rock.weak_against:
+    def fight(self,other_figure: 'Figure'):
+        if self.figure_name in other_figure.strong_against:
             return Result.LOSS
-        else:
-            raise RuntimeError('Unrecognized figure')
-
-
-class Paper:
-    figure_name = FigureName.PAPER.value
-    strong_against = [FigureName.ROCK.value]
-    weak_against = [FigureName.SCISSORS.value]
-
-    def fight(self,other_figure):
-        if other_figure in Paper.strong_against:
+        elif other_figure.figure_name in self.strong_against:
             return Result.WIN
-        elif other_figure == Paper.figure_name:
+        elif other_figure.figure_name == self.figure_name:
             return Result.TIE
-        elif other_figure in Paper.weak_against:
-            return Result.LOSS
         else:
-            raise RuntimeError('Unrecognized figure')
+            raise RuntimeError("Unrecognized figure")
 
 
-class Scissors:
-    figure_name = FigureName.SCISSORS.value
-    strong_against = [FigureName.PAPER.value]
-    weak_against = [FigureName.ROCK.value]
+class Rock(Figure):
+    figure_name = FigureName.ROCK
+    strong_against = [FigureName.SCISSORS]
 
-    def fight(self,other_figure):
-        if other_figure in Scissors.strong_against:
-            return Result.WIN
-        elif other_figure == Scissors.figure_name:
-            return Result.TIE
-        elif other_figure in Scissors.weak_against:
-            return Result.LOSS
-        else:
-            raise RuntimeError('Unrecognized figure')
+class Paper(Figure):
+    figure_name = FigureName.PAPER
+    strong_against = [FigureName.ROCK]
+
+class Scissors(Figure):
+    figure_name = FigureName.SCISSORS
+    strong_against = [FigureName.PAPER]
 
 
-rock = Rock()
-paper = Paper()
-scissors = Scissors()
-
-print(paper.fight('ROCK'))
-print(rock.fight("ROCK"))
-print(scissors.fight('ROCK'))
+print(Rock().fight(Paper()))
